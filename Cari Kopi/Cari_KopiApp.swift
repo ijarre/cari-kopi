052a80b271e8e9dab68baac43a291395bebfe851
Faizhar Riskisyah <mohamad.faizhar@gmail.com>
2022-11-19 16:12:24 +0700

    Initial Commit

---
+++ Cari Kopi/Cari_KopiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cari_KopiApp.swift
+//  Cari Kopi
+//
+//  Created by Faizhar Riskisyah on 19/11/22.
+//
+
+import SwiftUI
+
+@main
+struct Cari_KopiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
